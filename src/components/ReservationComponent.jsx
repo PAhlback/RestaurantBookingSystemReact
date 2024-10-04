2c7e58cbbb960c47c31d7035410f3826ca4c21a5
PAhlback <pontus.ahlback93@gmail.com>
2024-10-04 14:00:24 +0200

    add conditional rendering after making reservation to display confirmation or error

--- src/components/ReservationComponent.jsx
+++ src/components/ReservationComponent.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const ReservationComponent = () => {
     const [loading, setLoading] = useState(true);
+    const [callFailed, setCallFailed] = useState(false);
+    const [callSucceeded, setCallSucceeded] = useState(false);
+    const navigate = useNavigate();
     const location = useLocation();
     const { numberOfGuests, dateAndTime, customerEmail, customerName, customerPhone } = location.state || {};
 
@@ -14,19 +18,26 @@ const ReservationComponent = () => {
 
     async function postReservationRequest() {
         try {
+            const utcDateAndTime = new Date(dateAndTime).toISOString();
+
+            console.log("PostRequest utc date: ", utcDateAndTime)
+
             const response = await axios.post('https://localhost:7168/api/Reservations', {
                 numberOfGuests: numberOfGuests,
-                dateAndTime: dateAndTime,
+                dateAndTime: utcDateAndTime,
                 customerEmail: customerEmail,
                 customerName: customerName,
-                customerPhone: customerPhone
+                customerPhone: customerPhone,
+                utcTime: true
             });
-            console.log(response.data);
+            console.log("response data: ", response.data);
             setLoading(false);
+            setCallSucceeded(true);
         }
         catch (error) {
             console.error('error posting reservation:', error)
             setLoading(false);
+            setCallFailed(true);
         }
     }
 
@@ -36,11 +47,22 @@ const ReservationComponent = () => {
 
     return (
         <div>
-            <h1>Reservation Confirmed</h1>
-            <p>Your reservation for {numberOfGuests} guests on {dateAndTime.toString()} has been successfully made!</p>
-            <p>Name: {customerName}</p>
-            <p>Email: {customerEmail}</p>
-            <p>Phone: {customerPhone}</p>
+            {!loading && callSucceeded && (
+                <div>
+                    <h2>Reservation Confirmed</h2>
+                    <p>Your reservation for {numberOfGuests} guests on {dateAndTime.toString()} has been successfully made!</p>
+                    <p>Name: {customerName}</p>
+                    <p>Email: {customerEmail}</p>
+                    <p>Phone: {customerPhone}</p>
+                </div>
+            )}
+            {!loading && callFailed && !callSucceeded && (
+                <div>
+                    <h2>Reservation unsuccessful</h2>
+                    <p>Something went wrong. Please try again.</p>
+                    <button className='btn btn-outline-primary' onClick={() => navigate('/')}>Back to Start</button>
+                </div>
+            )}
         </div>
     )
 }
--- src/components/SelectTimeComponent.jsx
+++ src/components/SelectTimeComponent.jsx
@@ -5,20 +5,6 @@ const SelectTimeComponent = ({ openingTime, closingTime, interval, numberOfGuest
     const [selectedTime, setSelectedTime] = useState(null);
     const [availableSlots, setAvailableSlots] = useState([]);
 
-    const generateTimeSlots = (openingTime, closingTime, interval) => {
-        const slots = [];
-        let currentTime = new Date(openingTime);
-        const endTime = new Date(closingTime);
-
-        while (currentTime < endTime) {
-            const formattedTime = currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-            slots.push(formattedTime);
-            currentTime = new Date(currentTime.getTime() + interval * 60000);
-        }
-
-        return slots;
-    }
-
     const fetchAvailableSlots = async (dateAndTime) => {
         try {
             const formattedDate = new Date(dateAndTime).toLocaleString('sv-SE', {
@@ -31,10 +17,9 @@ const SelectTimeComponent = ({ openingTime, closingTime, interval, numberOfGuest
 
             const response = await axios.get(`https://localhost:7168/api/Tables/check-for-available-tables/${formattedDate}/${numberOfGuests}`);
             if (response.data) {
-                // Assume the API returns an array of available time slots
-                setAvailableSlots(response.data.map(slot => slot.time)); // Adjust according to your API response structure
+                setAvailableSlots(response.data.availableTimeSlots);
             }
-            console.log("API RESPONSE ", response.data);
+            console.log("API RESPONSE ", response.data.availableTimeSlots);
         } catch (error) {
             console.error("Error fetching available slots:", error);
         }
@@ -49,20 +34,16 @@ const SelectTimeComponent = ({ openingTime, closingTime, interval, numberOfGuest
     }, [selectedDate, numberOfGuests]);
 
     const TimeSlotPicker = () => {
-        const timeSlots = generateTimeSlots(openingTime, closingTime, interval);
-
         return (
             <div className='time-slot-picker'>
-                {timeSlots.map((slot, index) => (
-                    availableSlots.includes(slot) && ( // Only show available slots
-                        <button 
-                            key={index} 
-                            className={`time-slot-btn ${selectedTime === slot ? 'selected' : ''}`}
-                            onClick={() => setSelectedTime(slot)}
-                        >
-                            {slot}
-                        </button>
-                    )
+                {availableSlots.map((slot, index) => (
+                    <button
+                        key={index}
+                        className={`time-slot-btn ${selectedTime === slot ? 'selected' : ''}`}
+                        onClick={() => setSelectedTime(slot)}
+                    >
+                        {slot}
+                    </button>
                 ))}
             </div>
         );
